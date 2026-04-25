745a46a4b6a773021b0c9536a28ce901281f9a5a
Bogdan <kostin052006@gmail.com>
2026-04-25 21:40:01 +0300

    chore: вынесен API ключ в .env и обновлен .gitignore

--- src/API/MovieService.js
+++ src/API/MovieService.js
@@ -3,14 +3,18 @@ import axios from "axios";
 export default class MovieService {
   static async getMovies(page) {
     const response = await axios.get(
-      `https://api.themoviedb.org/3/movie/popular?api_key=792d42b959dcf4279f988bd6b9e5cff6&language=ru-RU&page=${page}`
+      `https://api.themoviedb.org/3/movie/popular?api_key=${
+        import.meta.env.VITE_TMDB_API_KEY
+      }&language=ru-RU&page=${page}`
     );
     return response.data.results;
   }
 
   static async getMovie(id) {
     const response = await axios.get(
-      `https://api.themoviedb.org/3/movie/${id}?api_key=792d42b959dcf4279f988bd6b9e5cff6&language=ru-RU`
+      `https://api.themoviedb.org/3/movie/${id}?api_key=${
+        import.meta.env.VITE_TMDB_API_KEY
+      }&language=ru-RU`
     );
     return response.data;
   }
